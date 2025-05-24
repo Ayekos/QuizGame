3b5d77f2bfab4e3bfa42d0057bdaa190cda0c648
Ayekos <veghniko23@gmail.com>
2025-05-24 16:50:57 +0200

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,19 @@
+﻿open Quiz
+open System
+
+[<EntryPoint>]
+let main argv =
+    let questions = loadQuestions "questions.json" |> shuffleQuestions |> List.truncate 10
+    Console.ForegroundColor <- ConsoleColor.Cyan
+    printfn "Welcome to Quiz Game!"
+    Console.ResetColor()
+
+    let score = 
+        questions 
+        |> List.map askQuestion 
+        |> List.sum
+
+    Console.ForegroundColor <- ConsoleColor.Blue
+    printfn "\nGame over! Score: %d/%d\n" score questions.Length
+    Console.ResetColor()
+    0
